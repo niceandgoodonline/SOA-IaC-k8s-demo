72739fed61cd45e2c84d013c6a808eff4ca6d71c
niceAndGoodOnline <felix.ruttan@gmail.com>
2022-06-14 15:52:52 -0700

    Pushed with gittyup

--- terraform/vpc/state.tf
+++ terraform/vpc/state.tf
@@ -6,12 +6,12 @@ resource "aws_ssm_parameter" "vpc_id_state" {
 
 resource "aws_ssm_parameter" "vpc_private_subnets_state" {
   name  = "/terramera-demo/state/vpc/private_subnets"
-  type  = "String"
-  value = module.vpc.private_subnets
+  type  = "StringList"
+  value = join(",", module.vpc.private_subnets)
 }
 
 resource "aws_ssm_parameter" "vpc_public_subnets_state" {
   name  = "/terramera-demo/state/vpc/public_subnets"
-  type  = "String"
-  value = module.vpc.public_subnets
+  type  = "StringList"
+  value = join(",", module.vpc.public_subnets)
 }
